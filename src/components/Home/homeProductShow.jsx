d3c4fb7d28d092f61d12eb0aa8f93dbb9d3a0acc
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-08-24 01:21:23 +0530

    frontend

--- src/components/Home/homeProductShow.jsx
+++ src/components/Home/homeProductShow.jsx
@@ -1,7 +1,76 @@
 import React from 'react'
 import './homeComponent.css'
+import demoImg from "../../assets/images/product/demo-product.jpg"
+// import star from "../../assets/logo/star.png"
 
 const homeProductShow = () => {
+  
+  //Sweet Amla , Ghee Amla , Gandharaaj Amla , Sweet Tangy , Sweet Chili Tangy , Extra Chili Tangy , Imli Achaar
+  const products = [
+    {
+      name: 'Sweet Amla',
+      image: demoImg,
+      link: '/our-products/sweet-amla',
+      price: '100',
+      ratings: '0',
+      quantity: '100',
+      tagLine: 'Sweet Amla'
+    },
+    {
+      name: 'Ghee Amla',
+      image: demoImg,
+      link: '/our-products/ghee-amla',
+      price: '100',
+      ratings: '0',
+      quantity: '100',
+      tagLine: 'Ghee Amla'
+    },
+    {
+      name: 'Gandharaaj Amla',
+      image: demoImg,
+      link: '/our-products/gandharaaj-amla',
+      price: '100',
+      ratings: '0',
+      quantity: '100',
+      tagLine: 'Gandharaaj Amla'
+    },
+    {
+      name: 'Sweet Tangy',
+      image: demoImg,
+      link: '/our-products/sweet-tangy',
+      price: '100',
+      ratings: '0',
+      quantity: '100',
+      tagLine: 'Sweet Tangy'
+    },
+    {
+      name: 'Sweet Chili Tangy',
+      image: demoImg,
+      link: '/our-products/sweet-chili-tangy',
+      price: '100',
+      ratings: '0',
+      quantity: '100',
+      tagLine: 'Sweet Chili Tangy'
+    },
+    {
+      name: 'Extra Chili Tangy',
+      image: demoImg,
+      link: '/our-products/extra-chili-tangy',
+      price: '100',
+      ratings: '0',
+      quantity: '100',
+      tagLine: 'Extra Chili Tangy'
+    },
+    {
+      name: 'Imli Achaar',
+      image: demoImg,
+      link: '/our-products/imli-achaar',
+      price: '100',
+      ratings: '0',
+      quantity: '100',
+      tagLine: 'Imli Achaar'
+    }
+  ]
 
   return (
     <div>
@@ -14,10 +83,17 @@ const homeProductShow = () => {
             </div>
             <div className='PS-product-grid-container'>
               <div className='carrousel'>
-
-                <div className='card bg-pl'>
-                  <p>Niladri</p>
-                </div>
+                
+                {products.map((product, index) => (
+                  <a href={product.link}>
+                    <div className="card">
+                      <div className="card-product-image">
+                        <img src={product.image} alt={product.name} />
+                      </div>
+                      <h3 className="card-product-name c-b">{product.name}</h3>
+                    </div>
+                  </a>
+                ))}
 
               </div>
             </div>
